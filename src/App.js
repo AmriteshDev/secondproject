c78b5eb7ebfa836dc802a762988f511c9626fe4c
Amriteshsingh94 <amriteshsingh2000n@gmail.com>
2023-12-28 15:50:31 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Button } from "@chakra-ui/react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <h1>dfde</h1>
+
+    <Router>
+      <Header />
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = ()=> {
+    return (
+        <div>fdlj</div>
+    );
+}
+
+export default Header;
